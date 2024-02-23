b049b90e9e8cfc010e46c0d8839d71a3516e9234
emiliaw1 <124522957+emiliaw1@users.noreply.github.com>
2024-02-23 22:48:29 +0100

    Create struktury.cpp

---
+++ struktury_1611/struktury.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Uczen {
+	string imie;
+	string nazwisko;
+	int pkt = 100;
+	bool plec;
+}
+
+void plec() {
+	if(imie_ucznia =)
+}
+
+void dodaj_pkt(int pkt_dodane) {
+	this -> pkt += pkt_dodane;
+}
+
+struct Klasa {
+	vector<Uczen> uczniowie;
+	string nazwa;
+	
+void liczba_uczniow() {
+		
+}
+
+struct Szkola {
+
+}
+
+int main() {
+	Uczen uczen1 = ('Maupa', 'Palińska', 'k');
+	Uczen uczen2 = ('Tymus', 'Siekierka', 'm');
+
+	Klasa klasa1 = ('1d');
+	Klasa klasa2 = ('2d');
+
+	klasa.uczniowie.push_back(uczen1);
+	klasa.uczniowie.push_back(uczen2);
+
+	klasa uczniowie[0];
+
+
+	return 0;
+}
+// uczen - imie nazwisko liczba pkt 100 plec, konstruktor imie_nazwisko dodajpkt
+// klasa - vector<uczen> nazwa, iloscuczniow() sredniapkt() wypisz uczniow()
+// szkola - vector<klasa> nazwa, iloscuczniow() wypiszklasy()
+// pokazuje sie najpierw lista szkol, potem wybor szkoly, lista klas w tej szkole, wybor klasy, uczniowie w tej klasie, zeby powrocic to np 0
+// nie robic cout jak nie ma w tresci zadania
