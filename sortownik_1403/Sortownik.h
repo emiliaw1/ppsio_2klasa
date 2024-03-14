ea9c821549be5f21fd498e3b7c9ec70b3cebc45b
emiliaw1 <124522957+emiliaw1@users.noreply.github.com>
2024-03-14 13:22:14 +0100

    Create Sortownik.h

---
+++ sortownik_1403/Sortownik.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+using namespace std;
+class Sortownik {
+	static vector<int> bubble_sort(vector<int>);
+	static vector<int> insert_sort(vector<int>);
+	static vector<int> selection_sort(vector<int>);
+};
