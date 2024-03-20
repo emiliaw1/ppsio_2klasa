46f3e5a4fda64724c77019d065a242bbe21e436d
emiliaw1 <124522957+emiliaw1@users.noreply.github.com>
2024-03-20 20:47:41 +0100

    Update Sortownik.h

--- sortownik_1403/Sortownik.h
+++ sortownik_1403/Sortownik.h
@@ -1,8 +1,9 @@
-#pragma once
 #include <vector>
+#pragma once
 using namespace std;
-class Sortownik {
+class Sortownik{
+public:
 	static vector<int> bubble_sort(vector<int>);
-	static vector<int> insert_sort(vector<int>);
+	static vector<int> insertion_sort(vector<int>);
 	static vector<int> selection_sort(vector<int>);
 };
