13b540b28d3f6e6c0cf4f059e5787848066ed836
emiliaw1 <124522957+emiliaw1@users.noreply.github.com>
2024-03-21 13:30:23 +0100

    Create Rysownik.h

---
+++ rysownik_2103/Rysownik.h
@@ -0,0 +1,7 @@
+#pragma once
+using namespace std;
+class Rysownik{
+public:
+	void RysujProstokat(struct pos, struct size, int kolor, int wypelnienie);
+	void RysujKolo(struct pos, struct r, int kolor, int wypelnienie);
+};
