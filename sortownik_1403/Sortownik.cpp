0092f90ebd8dbadebe7f8c0fd03f876e670a4bde
emiliaw1 <124522957+emiliaw1@users.noreply.github.com>
2024-03-21 07:22:06 +0100

    Update Sortownik.cpp
    Przekazywanie vectora przez wskazniki

--- sortownik_1403/Sortownik.cpp
+++ sortownik_1403/Sortownik.cpp
@@ -4,63 +4,63 @@
 
 using namespace std;
 
-vector<int> Sortownik::bubble_sort(vector<int> arr)
+vector<int> Sortownik::bubble_sort(vector<int> * arr)
 {
-    int n = arr.size();
+    int n = arr->size();
     for (int i = 0; i < n - 1; ++i)
     {
         for (int j = 0; j < n - i - 1; ++j)
         {
-            if (arr[j] > arr[j + 1])
+            if (arr->at(j) > arr->at(j + 1))
             {
-                swap(arr[j], arr[j + 1]);
+                swap(arr->at(j), arr->at(j + 1));
             }
         }
     }
-    return arr;
+    return *arr;
 }
 
-vector<int> Sortownik::insertion_sort(vector<int> arr)
+vector<int> Sortownik::insertion_sort(vector<int> * arr)
 {
-    int n = arr.size();
+    int n = arr->size();
     for (int i = 1; i < n; ++i)
     {
-        int key = arr[i];
+        int key = arr->at(i);
         int j = i - 1;
 
-        while (j >= 0 && arr[j] > key)
+        while (j >= 0 && arr->at(j) > key)
         {
-            arr[j + 1] = arr[j];
+            arr->at(j + 1) = arr->at(j);
             j = j - 1;
         }
-        arr[j + 1] = key;
+        arr->at(j + 1) = key;
     }
 
-    return arr;
+    return *arr;
 }
 
-vector<int> Sortownik::selection_sort(vector<int> arr)
+vector<int> Sortownik::selection_sort(vector<int> * arr)
 {
-    int n = arr.size();
+    int n = arr->size();
     for (int i = 0; i < n - 1; ++i)
     {
         int minIndex = i;
         for (int j = i + 1; j < n; ++j)
         {
-            if (arr[j] < arr[minIndex])
+            if (arr->at(j) < arr->at(minIndex))
             {
                 minIndex = j;
             }
         }
-        swap(arr[minIndex], arr[i]);
+        swap(arr->at(minIndex), arr->at(i));
     }
 
-    return arr;
+    return *arr;
 }
 
-void print_vector(vector<int> arr) 
+void print_vector(vector<int> * arr) 
 {
-    for (int i : arr)
+    for (int i : *arr)
     {
         cout << i << " ";
     }
@@ -72,19 +72,19 @@ int main()
 {
     vector<int> arr = {5, 2, 7, 1, 9, 3};
     cout << "Wektor nieposortowany: ";
-    print_vector(arr);
+    print_vector(&arr);
 
-    vector<int> output_bubble_sort = Sortownik::bubble_sort(arr);
+    vector<int> output_bubble_sort = Sortownik::bubble_sort(&arr);
     cout << "Wektor posortowany przez bubble_sort: ";
-    print_vector(output_bubble_sort);
+    print_vector(&output_bubble_sort);
 
-    vector<int> output_insertion_sort = Sortownik::insertion_sort(arr);
+    vector<int> output_insertion_sort = Sortownik::insertion_sort(&arr);
     cout << "Wektor posortowany przez insertion_sort: ";
-    print_vector(output_insertion_sort);
+    print_vector(&output_insertion_sort);
 
-    vector<int> output_selection_sort = Sortownik::selection_sort(arr);
+    vector<int> output_selection_sort = Sortownik::selection_sort(&arr);
     cout << "Wektor posortowany przez selection_sort: ";
-    print_vector(output_selection_sort);
+    print_vector(&output_selection_sort);
 
     return 0;
 }
