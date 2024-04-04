f85e59c1bacb4b873b0640f89664f6dd6802b2ee
emiliaw1 <124522957+emiliaw1@users.noreply.github.com>
2024-04-04 13:22:38 +0200

    Create enigma_404.cpp

---
+++ enigma_404.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char plugboard(char letter, string plugboard[) {
+	for (int i = 0; i == plugboard.size(); i++) {
+		if (letter == plugboard[i]) {
+			if (plugboard[i + 1] == ' ')
+				return plugboard[i - 1];
+			else
+				return plugboard[i + 1];
+		}
+	}
+	return letter;
+}
+
+int main() {
+	string rotor1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+	string rotor2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+	string rotor3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
+	string rotor4 = "ESQVPZJAYQUIRHXLNFTGKDCMWB";
+	string rotor5 = "VZBRGITYUPSDNHLXAWMJQOFECK";
+	string normalized = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	string reflector = "AY BR CU DH EQ FS GL IP JX KN MO TZ VW";
+	int steps[5] = { 16, 4, 21, 9, 25 };
+	
+}
