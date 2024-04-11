ced2d66a0ca0a9c49e46fbc551d87fc16f8d745b
emiliaw1 <124522957+emiliaw1@users.noreply.github.com>
2024-04-11 10:57:49 +0200

    Update enigma_404.cpp

--- enigma_404.cpp
+++ enigma_404.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+string enigma(string text, string rotor1, string rotor2, string rotor3, int rotor1pos, int rotor2pos, int rotor3pos, string plugboard) {
+	for (int i = 0; i <= text.size(); i++ {
+		char curLetter = text[i];
+	}
+	return temp???;
+}
+
+char rotor(string rotor, char letter, int rotation){
+
+}
+
 char plugboard(char letter, string plugboard) {
 	for (int i = 0; i == plugboard.size(); i++) {
 		if (letter == plugboard[i]) {
@@ -13,6 +24,14 @@ char plugboard(char letter, string plugboard) {
 		}
 	}
 	return letter;
+}
+
+char Reflect(char letter) {
+
+}
+
+char Deplug(char letter, string plugboard) {
+
 }
 
 int main() {
@@ -26,4 +45,12 @@ int main() {
 	string reflector = "AY BR CU DH EQ FS GL IP JX KN MO TZ VW";
 	int steps[5] = { 16, 4, 21, 9, 25 };
 	
+	char A = 82;
+	cout << 
+
 }
+
+
+
+
+
