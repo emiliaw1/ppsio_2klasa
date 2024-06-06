b3713674d31d60baa6b4298c5f92f4c04f81d8df
emiliaw1 <124522957+emiliaw1@users.noreply.github.com>
2024-06-06 13:44:47 +0200

    Create main.cpp

---
+++ user_606/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string kategoria = "Sci-Fi, Horror, Fantasy, Thriller, Documentary, Romance, Comedy";
+	User Emilia(1, Emilia);
+	User.dodaj_kategorie();
+	User.wyswietl_kategorie;
+
+
+}
